b4971ac7abc545de7093efad7041384b83a9cf38
Vastarelli, Michael <michael.vastarelli@changehealthcare.com>
2020-05-04 12:24:55 -0400

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Sub(int a, int b) => a - b;
+        public int Mul(int a, int b) => a * b;
+        public int Div(int a, int b) => a / b;
+    }
+}
